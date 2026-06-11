2c27e46f5b39d759e87a43b2fca714f5bbd091a4
UttamGiri <uttamgiri32@gmail.com>
2026-06-10 21:35:05 -0400

    fixed

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -2,6 +2,7 @@ locals {
   github_repository = "${var.github_org}/${var.github_repo}"
   terraform_roles = [
     "roles/serviceusage.serviceUsageAdmin",
+    "roles/storage.admin",
     "roles/logging.logWriter",
     "roles/resourcemanager.projectIamAdmin",
     "roles/iam.serviceAccountAdmin",
